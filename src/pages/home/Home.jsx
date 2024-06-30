45c05eb980f583aaa211c2a68a0d4b7d8748dd0c
phgoodw <pattygsocials@gmail.com>
2024-06-30 13:26:41 -0400

    Test 3

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import text from '../../config/text.json';
 import "./Home.css";
 
 const Home = () => {
+
+	useEffect(() => {
+		const images = [
+			"https://i.imgur.com/N7XsoR3.jpeg",
+			"https://i.imgur.com/yeJiq5G.jpeg",
+			"https://i.imgur.com/ckURYig.jpeg",
+			"https://i.imgur.com/hVx74Ve.jpeg",
+			"https://i.imgur.com/oU0Ma5E.jpeg",
+			"https://i.imgur.com/N7XsoR3.jpeg"
+		];
+
+		const preloadImages = () => {
+			images.forEach((image) => {
+				const img = new Image();
+				img.src = image;
+				img.className = 'hidden';
+				document.body.appendChild(img);
+			});
+		};
+
+		let currentIndex = 0;
+		const slideshow = document.getElementById('slideshow');
+
+		const changeImage = () => {
+			slideshow.style.backgroundImage = `url(${images[currentIndex]})`;
+			currentIndex = (currentIndex + 1) % images.length;
+		};
+
+		preloadImages();
+		changeImage();
+		const interval = setInterval(changeImage, 18000); // Change image every 18 seconds
+
+		return () => clearInterval(interval); // Cleanup interval on component unmount
+	}, []);
+
+
 	return (
 		<HelmetProvider>
 			<section id="home" className="home">
@@ -14,9 +50,7 @@ const Home = () => {
 					<meta name="description" content={text.meta.description} />
 				</Helmet>
 				<div className="intro_sec d-block d-lg-flex align-items-center">
-					<div
-						className="h_bg-image order-1 order-lg-2 h-100 slides" style={{ borderRadius: '4rem' }}
-					>
+					<div id="slideshow" className="h_bg-image order-1 order-lg-2 h-100" style={{ borderRadius: '4rem' }}>
 					</div>
 					<div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
 						<div className="align-self-center ">
