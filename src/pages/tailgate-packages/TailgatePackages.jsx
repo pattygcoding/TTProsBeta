a5d369630c29060705192a8cf4106c2ece918fa3
phgoodw <pattygsocials@gmail.com>
2024-07-04 16:50:14 -0400

    Oops mb

--- src/pages/tailgate-packages/TailgatePackages.jsx
+++ src/pages/tailgate-packages/TailgatePackages.jsx
@@ -9,7 +9,7 @@ import { ParkingForms } from "./parking-forms";
 import { AddOnSelection } from './add-on-selection';
 import { TabLabel } from "../../components/tab-label";
 import { PageTitle } from "../../components/page-title";
-import email from '../../config/email_test.json';
+import email from '../../config/email.json';
 import t from '../../config/text.json';
 import "./TailgatePackages.css";
 import { AdditionalInfoForms } from "./additional-info-forms";
