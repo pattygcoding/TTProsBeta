6008b0d78901b767d0096861601dee90672c8315
phgoodw <pattygsocials@gmail.com>
2024-07-09 17:53:54 -0400

    On second thought, I'll just stick with JS

--- src/pages/tailgate-packages/TailgatePackages.jsx
+++ src/pages/tailgate-packages/TailgatePackages.jsx
@@ -9,46 +9,14 @@ import { ParkingForms } from "./parking-forms";
 import { AddOnSelection } from './add-on-selection';
 import { TabLabel } from "@/components/tab-label";
 import { PageTitle } from "@/components/page-title";
+import { initialFormData } from "./TailgatePackages.util";
 import email from '@/config/email.json';
 import t from '@/config/text.json';
 import "./TailgatePackages.css";
 import { AdditionalInfoForms } from "./additional-info-forms";
 
 const TailgatePackages = () => {
-	const [formData, setFormData] = useState({
-		first_name: "",
-		last_name: "",
-		email: "",
-		phone_number: "",
-		include_season: "",
-		include_game_one: "",
-		include_game_two: "",
-		include_game_three: "",
-		include_game_four: "",
-		include_game_five: "",
-		include_game_six: "",
-		include_game_seven: "",
-		include_cooler: "",
-		include_chair: "",
-		include_table: "",
-		include_tent: "",
-		include_cocktail_table: "",
-		include_side_tent: "",
-		cooler_amount: 1,
-		chair_amount: 1,
-		table_amount: 1,
-		tent_amount: 1,
-		cocktail_table_amount: 1,
-		side_tent_amount: 1,
-		lot_number: "",
-		spot_number: "",
-		additional_comment: "",
-		hear_about_us_question: "",
-		loading: false,
-		show: false,
-		alertmessage: "",
-		variant: "",
-	});
+	const [formData, setFormData] = useState(initialFormData);
 
 	const [selectedPackageType, setSelectedPackageType] = useState('cub');
 
