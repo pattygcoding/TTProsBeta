eab6878c893bdcdf5c48aed80e3ad9832a7be6fe
phgoodw <pattygsocials@gmail.com>
2024-07-09 12:21:28 -0400

    Gallery cleanup

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -9,14 +9,27 @@ import "./Gallery.css";
 
 const Gallery = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [transitioning, setTransitioning] = useState(false);
     const images = Object.values(image.gallery);
 
     const handlePrevClick = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+        if (transitioning) return;
+        setTransitioning(true);
+        const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+        setTimeout(() => {
+            setCurrentIndex(newIndex);
+            setTransitioning(false);
+        }, 300);
     };
 
     const handleNextClick = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        if (transitioning) return;
+        setTransitioning(true);
+        const newIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+        setTimeout(() => {
+            setCurrentIndex(newIndex);
+            setTransitioning(false);
+        }, 300);
     };
 
     return (
@@ -24,13 +37,20 @@ const Gallery = () => {
             <Container className="Gallery-header">
                 <TabLabel label={t.gallery.title} />
                 <Col>
-                    <PageTitle title={t.gallery.title}/>
+                    <PageTitle title={t.gallery.title} />
                     <Row className="sec_sp justify-content-center align-items-center">
                         <Col xs="auto">
                             <Button className="gallery-button" onClick={handlePrevClick}>←</Button>
                         </Col>
-                        <Col xs="auto">
-                            <img src={images[currentIndex]} alt={`Gallery item ${currentIndex + 1}`} className="img-fluid gallery-image" style={{ borderRadius: '4rem' }}  />
+                        <Col xs="auto" className="image-container">
+                            <div className={`gallery-slide ${transitioning ? 'transition' : ''}`}>
+                                <img
+                                    src={images[currentIndex]}
+                                    alt={`Gallery item ${currentIndex + 1}`}
+                                    className="img-fluid gallery-image"
+                                    style={{ borderRadius: '4rem' }}
+                                />
+                            </div>
                         </Col>
                         <Col xs="auto">
                             <Button className="gallery-button" onClick={handleNextClick}>→</Button>
