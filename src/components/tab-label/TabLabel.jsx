7361e4fa7a487da1519e33ed6a8fda467e6a5c5a
phgoodw <pattygsocials@gmail.com>
2024-07-21 16:17:59 -0400

    GA Test

--- src/components/tab-label/TabLabel.jsx
+++ src/components/tab-label/TabLabel.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import t from '@/config/text.json';
+import ga from '@/config/ga.json';
 import { Helmet } from "react-helmet-async";
 
 const TabLabel = ({ label }) => {
     return (
         <Helmet>
+            <script async src={ga.url}></script>
+            <script>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${ga.tag}');
+                `}
+            </script>
             <meta charSet="utf-8" />
             <title>{label} | {t.meta.title}</title>
             <meta name="description" content={t.meta.description} />
