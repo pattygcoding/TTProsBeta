90097c930de83ee110d45358f37cd62eb3ee9423
phgoodw <pattygsocials@gmail.com>
2024-09-23 20:08:25 -0400

    Stanford update

--- src/pages/home/Home.utils.js
+++ src/pages/home/Home.utils.js
@@ -1,7 +1,16 @@
 export const images = [
     "https://i.imgur.com/N7XsoR3.jpeg",
-    "https://i.imgur.com/yeJiq5G.jpeg",
+    "https://i.imgur.com/eVcXirP.jpeg",
     "https://i.imgur.com/ckURYig.jpeg",
-    "https://i.imgur.com/hVx74Ve.jpeg",
-    "https://i.imgur.com/oU0Ma5E.jpeg"
+    "https://i.imgur.com/oU0Ma5E.jpeg",
+    "https://i.imgur.com/geXk9vR.jpeg",
+    "https://i.imgur.com/cq3FF9i.jpeg",
+    "https://i.imgur.com/ddkOWub.jpeg",
+    "https://i.imgur.com/PeAJAL5.jpeg",
+    "https://i.imgur.com/FtjvZwq.jpeg",
+    "https://i.imgur.com/mavckhm.jpeg",
+    "https://i.imgur.com/E2xL2zR.jpeg",
+    "https://i.imgur.com/lYj9T4j.jpeg",
+    "https://i.imgur.com/YeuS6N2.jpeg",
+    "https://i.imgur.com/BO2x9Nh.jpeg"
 ];
--- src/pages/tailgate-packages/TailgatePackages.utils.js
+++ src/pages/tailgate-packages/TailgatePackages.utils.js
@@ -3,59 +3,74 @@ import data from '@/config/datapack.json';
 import { gameIds } from '@/utils/utils.js';
 
 export function calculatePrice(selectedPackageType, formData) {
-    let packagePrices = {
-      add_ons: {},
-    };
-  
-    let priceField;
-    switch (selectedPackageType) {
-      case 'intruder':
-      case 'cub':
-        priceField = 'price_cub';
-        break;
-      case 'ultimate':
-      case 'vip':
-        priceField = 'price_vip';
-        break;
-      default:
-        console.error('ERROR: Could not determine package type.');
-        return 0;
-    }
-  
-    gameIds.forEach((game) => {
-      packagePrices[game] = t.packages[game][priceField];
-    });
-  
-    data.add_on_fields.forEach((addOnField) => {
-      packagePrices.add_ons[addOnField] = t.packages.add_ons[addOnField].price;
-    });
-  
-    let totalAmount = 0;
-  
-    gameIds.forEach((game) => {
-      if (formData[`include_${game}`]) {
-        totalAmount += packagePrices[game];
-      }
-    });
-  
-    data.add_on_fields.forEach((addOnField) => {
-      if (formData[`include_${addOnField}`]) {
-        const amount = formData[`${addOnField}_amount`];
-        totalAmount += packagePrices.add_ons[addOnField] * amount;
-      }
-    });
-  
-    return totalAmount;
-  }
+	let packagePrices = {
+		add_ons: {},
+	};
+
+	let priceField;
+	switch (selectedPackageType) {
+		case 'intruder':
+		case 'cub':
+		case 'standard':
+			priceField = 'price_standard';
+			break;
+		case 'ultimate':
+		case 'vip':
+		case 'premium':
+			priceField = 'price_premium';
+			break;
+		default:
+			console.error('ERROR: Could not determine package type.');
+			return 0;
+	}
+
+	// Initialize game prices
+	data.game_fields.forEach((game) => {
+		packagePrices[game.id] = game[priceField];
+	});
+
+	// Initialize add-on prices
+	data.add_on_fields.forEach((addOnField) => {
+		packagePrices.add_ons[addOnField] = t.packages.add_ons[addOnField].price;
+	});
+
+	let totalAmount = 0;
+	let numberOfGames = 0;
+
+	// Check if season is selected
+	if (formData.include_season) {
+		totalAmount += packagePrices['season'];
+		numberOfGames = 7; // Season includes all 7 games
+	} else {
+		// Calculate total for selected games and count them
+		data.game_fields.forEach((game) => {
+			if (formData[`include_${game.id}`]) {
+				totalAmount += packagePrices[game.id];
+				numberOfGames += 1;
+			}
+		});
+	}
+
+	// Calculate total for selected add-ons per game
+	data.add_on_fields.forEach((addOnField) => {
+		if (formData[`include_${addOnField}`]) {
+			const amount = formData[`${addOnField}_amount`] || 0;
+			const addOnPricePerGame = packagePrices.add_ons[addOnField] * amount;
+			totalAmount += addOnPricePerGame * numberOfGames;
+		}
+	});
+
+	return totalAmount;
+}
 
 export function isGameSelected(formData) {
-    return gameIds.some((game) => formData[`include_${game}`]);
+	return gameIds.some((game) => formData[`include_${game}`]);
 }
 
 export function validCountValues(formData) {
-    const amounts = data.add_on_fields.map((addOn) => 
-        formData[`include_${addOn}`] ? formData[`${addOn}_amount`] : null
-    );
+	const amounts = data.add_on_fields.map((addOn) =>
+		formData[`include_${addOn}`] ? formData[`${addOn}_amount`] : null
+	);
 
-    return amounts.every((amount) => amount === null || amount > 0);
+	return amounts.every((amount) => amount === null || amount > 0);
 }
